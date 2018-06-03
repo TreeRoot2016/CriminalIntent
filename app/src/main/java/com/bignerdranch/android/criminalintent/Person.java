1b8985866e5dd7d8556c45e63a11a3ee46ca35b8
tree <ltb_2014@126.com>
2018-06-03 18:56:39 +0800

    增加类

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Person.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.criminalintent;
+
+/**
+ * <pre>
+ *     author : tree_root
+ *     e-mail : xxx@xx
+ *     time   : 2018/06/03
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+public class Person {
+}
